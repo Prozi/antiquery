814c75beaf2f902ea58f2223a74d0fc8b3be1dd5
Jacek Pietal <prozi85@gmail.com>
2017-10-05 23:41:56 +0200

    update

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ export default function $(selector) {
   const divs = typeof selector === 'string' ?
     Array.prototype.slice.call(document.querySelectorAll(selector)) :
     Array.isArray(selector) ? (selector ? selector : []) : [selector]
-  const wrap = (fn) => divs.forEach(fn)
   const api = {
     parents: () => divs.map((el) => el.parentElement),
     children: () => divs.map((el) => el.children),
     find: (what) => divs.map((el) => el.querySelectorAll(what)),
-    remove: () => wrap((el) => el.parentElement.removeChild(el)),
-    addClass: (className) => wrap((el) => el.classList.add(className)),
-    removeClass: (className) => wrap((el) => el.classList.remove(className)),
+    remove: () => divs.forEach((el) => el.parentElement.removeChild(el)),
+    addClass: (className) => divs.map((el) => { if (el) el.classList.add(className); return el }),
+    removeClass: (className) => divs.map((el) => { if (el) el.classList.remove(className); return el }),
     animate: (animation, duration = 250) => {
       wrap((el) => {
         api.addClass(animation)
@@ -18,7 +17,7 @@ export default function $(selector) {
     },
     hide: () => divs.map((el) => { if (el) el.style.display = 'none'; return el }),
     show: () => divs.map((el) => { if (el) el.style.display = null; return el }),
-    each: wrap
+    each: (fn) => divs.forEach(fn)
   }
   divs.forEach((el, i) => api[i] = el)
   return api;
