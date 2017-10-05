b757ed5ffc492777e9ad0e166dcb33e62c9a0958
Jacek Pietal <prozi85@gmail.com>
2017-10-05 23:44:04 +0200

    update-fix

--- commonjs/index.js
+++ commonjs/index.js
@@ -2,22 +2,23 @@ exports.default = function $(selector) {
   const divs = typeof selector === 'string' ?
     Array.prototype.slice.call(document.querySelectorAll(selector)) :
     Array.isArray(selector) ? (selector ? selector : []) : [selector]
+  const each = divs.map.bind(divs)
   const api = {
-    parents: () => divs.map((el) => el.parentElement),
-    children: () => divs.map((el) => el.children),
-    find: (what) => divs.map((el) => el.querySelectorAll(what)),
+    parents: () => each((el) => el.parentElement),
+    children: () => each((el) => el.children),
+    find: (what) => each((el) => el.querySelectorAll(what)),
     remove: () => divs.forEach((el) => el.parentElement.removeChild(el)),
-    addClass: (className) => divs.map((el) => { if (el) el.classList.add(className); return el }),
-    removeClass: (className) => divs.map((el) => { if (el) el.classList.remove(className); return el }),
+    addClass: (className) => each((el) => { if (el) el.classList.add(className); return el }),
+    removeClass: (className) => each((el) => { if (el) el.classList.remove(className); return el }),
     animate: (animation, duration = 250) => {
       wrap((el) => {
         api.addClass(animation)
         setTimeout(() => api.removeClass(animation), duration)
       })
     },
-    hide: () => divs.map((el) => { if (el) el.style.display = 'none'; return el }),
-    show: () => divs.map((el) => { if (el) el.style.display = null; return el }),
-    each: (fn) => divs.forEach(fn)
+    hide: () => each((el) => { if (el) el.style.display = 'none'; return el }),
+    show: () => each((el) => { if (el) el.style.display = null; return el }),
+    each
   }
   divs.forEach((el, i) => api[i] = el)
   return api;
