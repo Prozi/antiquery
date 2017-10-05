8b4ba6c0fcfb8c07a765a5059445087308817f8a
Jacek Pietal <prozi85@gmail.com>
2017-10-05 22:42:58 +0200

    initial anti query commit

---
+++ index.js
@@ -0,0 +1,25 @@
+export default function $(selector) {
+  const divs = typeof selector === 'string' ?
+    Array.prototype.slice.call(document.querySelectorAll(selector)) :
+    Array.isArray(selector) ? selector : [selector]
+  const wrap = (fn) => divs.forEach(fn)
+  const api = {
+    parents: () => divs.map((el) => el.parentElement),
+    children: () => divs.map((el) => el.children),
+    find: (what) => divs.map((el) => el.querySelectorAll(what)),
+    remove: () => wrap((el) => el.parentElement.removeChild(el)),
+    addClass: (className) => wrap((el) => el.classList.add(className)),
+    removeClass: (className) => wrap((el) => el.classList.remove(className)),
+    animate: (animation, duration = 250) => {
+      wrap((el) => {
+        api.addClass(animation)
+        setTimeout(() => api.removeClass(animation), duration)
+      })
+    },
+    hide: () => wrap((el) => { el.style.display = 'none'; return el }),
+    show: () => wrap((el) => { el.style.display = null; return el }),
+    each: wrap
+  }
+  divs.forEach((el, i) => api[i] = el)
+  return api;
+}
