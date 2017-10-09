43c985a4f68a1419900256346f23ce39dfeeab35
Jacek Pietal <prozi85@gmail.com>
2017-10-09 02:21:29 +0200

    repo-on-hub

---
+++ README.md
@@ -0,0 +1,65 @@
+## Anti-Query
+
+You dont need any Big-Ass-Query that weights about 50KB, this library is enough for your needs (propably)
+
+## Whats this
+
+this is DOM manipulation language
+
+## Installation
+
+install by typing `yarn add antiquery --save # or npm install antiquery --save`
+
+## What is the usage
+
+to use as import/export module use
+
+```javascript
+import $ from 'antiquery'
+```
+
+for older javascript use:
+```javascript
+const $ = require('antiquery/commonjs').default
+```
+
+then you can use like
+
+```javascript
+$('div.golden')
+  .removeClass('golden')
+  .css({ background: 'black', color: 'white' })
+  .text('hello')
+  .animate('blink')
+  .show()
+```
+
+## What is ready?
+
+* what document.querySelector supports (a lot in current browsers)
+* chaining next methods
+* methods:
+  * get (index? or returns all)
+  * each (fn)
+  * parents
+  * children
+  * find
+  * remove
+  * addClass ('string')
+  * removeClass ('string')
+  * css ({})
+  * hide
+  * show
+  * text (text)
+  * html (html)
+  * animate (class, ms)
+
+animate adds css class for duration of ms
+
+## Enjoy
+
+by Jacek Pietal (github.com/Prozi)
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -1,24 +1,70 @@
-export default function $(selector) {
-  const divs = typeof selector === 'string' ?
-    Array.prototype.slice.call(document.querySelectorAll(selector)) :
-    Array.isArray(selector) ? (selector ? selector : []) : [selector]
-  const each = divs.map.bind(divs)
-  const api = {
-    parents: () => each((el) => el.parentElement),
-    children: () => each((el) => el.children),
-    find: (what) => each((el) => el.querySelectorAll(what)),
-    remove: () => divs.forEach((el) => el.parentElement.removeChild(el)),
-    addClass: (className) => each((el) => { if (el) el.classList.add(className); return el }),
-    removeClass: (className) => each((el) => { if (el) el.classList.remove(className); return el }),
-    animate: (animation, duration = 250) => each((el) => {
-      api.addClass(animation)
-      setTimeout(() => api.removeClass(animation), duration)
-      return el
-    }),
-    hide: () => each((el) => { if (el) el.style.display = 'none'; return el }),
-    show: () => each((el) => { if (el) el.style.display = null; return el }),
-    each
+export class AntiQuery {
+  constructor (selector) {
+    this.elements = typeof selector === 'string' ?
+      Array.prototype.slice.call(document.querySelectorAll(selector)) :
+      Array.isArray(selector) ? (selector ? selector : []) : [selector]
+  },
+  get (index) {
+    if (typeof index !== 'undefined') return this.elements[index]
+    return this.elements
   }
-  divs.forEach((el, i) => api[i] = el)
-  return api;
+  each (fn) {
+    return this.elements.map.apply(this, fn)
+  }
+  parents () {
+    return this.each((el) => $(el.parentElement))
+  },
+  children () {
+    return this.each((el) => el.children.map($))
+  },
+  find (what) {
+    return this.each((el) => $(what)),
+  }
+  remove () {
+    this.elements.forEach((el) => el.parentElement.removeChild(el))
+  },
+  addClass (className) {
+    return this.each((el) => {
+      if (el) el.classList.add(className); return $(el)
+    }),
+  }
+  removeClass (className) {
+    return this.each((el) => {
+      if (el) el.classList.remove(className); return $(el)
+    })
+  },
+  css (css) {
+    return this.each((el) => {
+      if (el) Object.keys(css).forEach((prop) => {
+        el.style[prop] = css[prop];        
+      })
+      return $(el)
+    })
+  },
+  hide () {
+    return this.css({ display: 'none' })
+  },
+  show () {
+    return this.css({ display: null })
+  },
+  text (text) {
+    return this.each((el) => {
+      if (el) el.textContent = text; return $(el)
+    })
+  },
+  html (html) {
+    return this.each((el) => {
+      if (el) el.innerHTML = html; return $(el)
+    })
+  },
+  animate (animation, duration = 250) {
+    return this.each((el) => {
+      setTimeout(() => $(el).removeClass(animation), duration)
+      return $(el).addClass(animation)
+    })
+  }
+}
+
+export default function $(selector) {
+  return new AntiQuery(selector)
 }
